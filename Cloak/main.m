273d2d2276feb6ae79d19b26f27a08d35fef9b2e
Victor Zhou <vzhou842@gmail.com>
2015-11-14 15:14:36 -0500

    first commit

---
+++ Cloak/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Cloak
+//
+//  Created by Victor Zhou on 11/14/15.
+//  Copyright © 2015 Victor Zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
