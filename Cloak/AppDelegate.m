c3db623df52d2d9dc6bb1ea29f03fef9d95dfc8d
Victor Zhou <vzhou842@gmail.com>
2015-11-15 03:36:18 -0500

    tab bar UI, added Constants header

--- Cloak/AppDelegate.m
+++ Cloak/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "Constants.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,12 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    [[UITabBarItem appearance] setTitleTextAttributes:@{ NSForegroundColorAttributeName : [UIColor whiteColor] }
+                                             forState:UIControlStateNormal];
+    [[UITabBarItem appearance] setTitleTextAttributes:@{ NSForegroundColorAttributeName : CLK_GREEN }
+                                             forState:UIControlStateSelected];
+    
     return YES;
 }
 
---
+++ Cloak/Constants.h
@@ -0,0 +1,14 @@
+//
+//  Constants.h
+//  Cloak
+//
+//  Created by Victor Zhou on 11/15/15.
+//  Copyright © 2015 Victor Zhou. All rights reserved.
+//
+
+#ifndef Constants_h
+#define Constants_h
+
+#define CLK_GREEN [UIColor colorWithRed:48/255.0 green:180/255.0 blue:80/255.0 alpha:1.0]
+
+#endif
