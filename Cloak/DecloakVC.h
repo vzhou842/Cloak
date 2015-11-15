8cd906ba0d3b7e4249210ea56615974b8f138df3
Victor Zhou <vzhou842@gmail.com>
2015-11-14 19:29:20 -0500

    added DecloakVC

---
+++ Cloak/DecloakVC.h
@@ -0,0 +1,13 @@
+//
+//  DecloakVC.h
+//  Cloak
+//
+//  Created by Victor Zhou on 11/14/15.
+//  Copyright © 2015 Victor Zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DecloakVC : UIViewController
+
+@end
